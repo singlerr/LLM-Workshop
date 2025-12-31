d0fdc45a19c88dda393f3f17729d9688dd12c5a8
Ryu Seowoong <singlerr@naver.com>
2025-12-31 15:15:39 +0900

    update

--- personal_copilot/training/setup.sh
+++ personal_copilot/training/setup.sh
@@ -1,4 +1,18 @@
-pip install -r requirements.txt
+pip install git+https://github.com/huggingface/transformers
+pip install git+https://github.com/huggingface/accelerate
+pip install git+https://github.com/huggingface/peft
+pip install trl
+pip install huggingface-hub
+pip install bitsandbytes
+pip install evaluate
+pip install datasets
+pip install einops
+pip install wandb
+pip install tiktoken
+pip install deepspeed
+pip install tqdm
+pip install safetensors
+pip install "unsloth[conda] @ git+https://github.com/unslothai/unsloth.git"
 pip uninstall -y ninja && pip install ninja
 pip install packaging
 pip install flash-attn --no-build-isolation
